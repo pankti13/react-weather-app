74fe36fc285a892af4a98f5833b7c1321e83c6c2
Pankti Salvi <you@example.com>
2024-01-19 20:31:20 +0530

    Built structure and provided styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React from 'react';
-import './App.css';
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather Application
+      <Weather />
+      <div className="footer">
+        <strong>
+          <a
+            href="https://github.com/pankti13/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-Source code
+          </a>{" "}
+        </strong>
+        by{" "}
+        <strong>
+          <a
+            href="https://github.com/pankti13"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Pankti Salvi
+          </a>{" "}
+        </strong>
+        and this project is hosted on{" "}
+        <strong>
+          <a
+            href="https://react-weather-made-easy.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </strong>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import "./Weather.css";
+
+export default function Weather(){
+    return (
+      <div className="container">
+        <div className="innerContainer">
+          <h1 className="heading">React Weather Made Easy</h1>
+          <form id="search-bar" className="rowFirst">
+            <input
+              type="text"
+              id="cityName"
+              className="search"
+              placeholder="Enter a city name"
+              autoComplete="off"
+              autoFocus="on"
+            />
+            <input type="submit" defaultValue="Search" className="btn" />
+          </form>
+          <div className="row RowSecond">
+            <div className="col-6 location">Patan, India</div>
+            <div className="col-6 location" id="dayDateTime">
+              <span>Last Updated: </span>
+              <span className="day">Monday,</span> {" "}
+              <span className="date">
+                May 31,
+              </span>{" "}
+              <span className="time">
+                1:30
+              </span>
+            </div>{" "}
+          </div>
+          <div className="row RowThird">
+            <div className="col-5 d-flex justify-content-center">
+              <img
+                src={`https://openweathermap.org/img/wn/10d@2x.png`}
+                alt="clear"
+                id="mainIcon"
+              />
+            </div>{" "}
+            <div className="col-6">
+              <div className="row embeddedRow1">
+                <div className="current">Currently</div>
+              </div>
+              <div className="row embeddedRow2">
+                <div className="col-6 DisplayTemp">
+                  <div className="tempWrapper">
+                    <h1 id="current-temp">20</h1>
+                    <span className="units">
+                      <a href="/" id="celsius-link">
+                        C
+                      </a>
+                      <span className="pipe">|</span>
+                      <a href="/" id="fahrenheit-link">
+                        F
+                      </a>
+                    </span>
+                  </div>
+                  <span id="maxMinTemp">
+                    20°C/4°C
+                  </span>
+                </div>{" "}
+                <div className="col-6 DisplayExtraInfo">
+                  <h4 id="description">Overcast Clouds</h4>
+                  <h6>
+                    <strong>Humidity: </strong>
+                    <span id="humid-data">4%</span>
+                  </h6>
+                  <h6>
+                    <strong>Wind Speed: </strong>
+                    <span id="wind-data">2m/s</span>
+                  </h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
